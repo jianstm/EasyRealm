16387f59f5c3386583bfc3f82b50e5fb7aadd241
XiangQiu Jin <xq.jin@aftership.com>
2020-04-08 14:33:58 +0800

    [feature]: use UpdatePolicy

--- EasyRealm/Classes/Save.swift
+++ EasyRealm/Classes/Save.swift
@@ -11,23 +11,23 @@ import RealmSwift
 
 extension EasyRealm where T: Object {
 
-  public func save(update: Bool = false) throws {
+  public func save(update: Realm.UpdatePolicy) throws {
     _ = try self.saved(update: update)
   }
 
-  public func saved(update: Bool = false) throws -> T {
+  public func saved(update: Realm.UpdatePolicy) throws -> T {
     return (self.isManaged) ? try managed_save(update: update) : try unmanaged_save(update: update)
   }
 
-  public func update() throws {
-    _ = (self.isManaged) ? try managed_save(update: true) : try unmanaged_save(update: true)
+  public func update(update: Realm.UpdatePolicy) throws {
+    _ = (self.isManaged) ? try managed_save(update: update) : try unmanaged_save(update: update)
   }
 
 }
 
 fileprivate extension EasyRealm where T: Object {
 
-  func managed_save(update: Bool) throws -> T {
+  func managed_save(update: Realm.UpdatePolicy) throws -> T {
     let ref = ThreadSafeReference(to: self.base)
     guard let rq = EasyRealmQueue() else {
       throw EasyRealmError.RealmQueueCantBeCreate
@@ -35,16 +35,16 @@ fileprivate extension EasyRealm where T: Object {
     return try rq.queue.sync {
       guard let object = rq.realm.resolve(ref) else { throw EasyRealmError.ObjectCantBeResolved }
       rq.realm.beginWrite()
-      let ret = rq.realm.create(T.self, value: object, update: .all)
+      let ret = rq.realm.create(T.self, value: object, update: update)
       try rq.realm.commitWrite()
       return ret
     }
   }
 
-  func unmanaged_save(update: Bool) throws -> T {
+  func unmanaged_save(update: Realm.UpdatePolicy) throws -> T {
     let realm = try Realm()
     realm.beginWrite()
-    let ret = realm.create(T.self, value: self.base, update: .all)
+    let ret = realm.create(T.self, value: self.base, update: update)
     try realm.commitWrite()
     return ret
   }
